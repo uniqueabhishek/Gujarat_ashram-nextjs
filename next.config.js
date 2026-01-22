83ac152b75ed43198bd148896ed2d16f1e3dce51
Abhishek Jagtap <abhishekjaa@gmail.com>
2026-01-22 20:00:03 +0530

    Turbopack Applied to Loading is faster now

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion', '@dnd-kit/core'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 }
 
 module.exports = nextConfig
